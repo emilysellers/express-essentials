7e0f35866d063c83eabd864dc9c9685b68ae517b
Emily Sellers <emilymelindasellers@gmail.com>
2025-12-03 16:02:46 -0800

    Add basic CRUD routes

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ import data from "./data/mockdata.json" assert { type: "json" };
 const app = express();
 
 const PORT = 3000;
+
+//GET
+app.get("/", (request, response) => {
+  response.send("This is a GET request at /");
+});
+
+//POST
+app.post("/create", (request, response) => {
+  response.send("This is POST request at /create");
+});
+
+//PUT
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit");
+});
+
+//DELETE
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete");
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
-  console.log(data);
+  //console.log(data);
 });
